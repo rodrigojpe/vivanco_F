8d4d67ab730889afefe66dd6b05dbe784ded6836
rodrigo <academiacp@gmail.com>
2019-01-08 22:44:01 -0300

    probando centos vivanco

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict'
 var mongoose = require('mongoose');
 var app = require('./server');
-var port = process.env.PORT || 3789;
+var port = process.env.PORT || 8080;
 mongoose.Promise = global.Promise;
 
 // mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://104.248.235.159:27017/vivanco', { useNewUrlParser: true  })
+mongoose.connect('mongodb://165.227.52.167:27017/vivanco', { useNewUrlParser: true  })
   .then(() =>{
     console.log('la conexion con vivancoBand se establecio correctamente...');
     app.listen(port , () =>{
