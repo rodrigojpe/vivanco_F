0cb82a2771f506455ae3e5426c1d71f6160d26a3
rodrigo <academiacp@gmail.com>
2019-07-19 17:49:14 -0400

    agregando rigovanco 1.8

--- server.js
+++ server.js
@@ -18,31 +18,31 @@ var contact = require('./routes/contact');
 
 
 // configurar cabeceras y cors
-// app.use((req, res, next)=>{
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-//     res.header('Access-Control-Allow-Methods','GET, POST, OPTIONS, PUT, DELETE');
-//     res.header('Allow','GET, POST, OPTIONS, PUT, DELETE');
-//     next();
+app.use((req, res, next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods','GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow','GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+// var cors = require('cors');
+//
+//
+// app.use(cors());
+//
+// app.get('/', function(req, res, next) {
+//    res.json({ msg: 'This is CORS-enabled for all origins!' });
 // });
-
-var cors = require('cors');
-
-
-app.use(cors());
-
-app.get('/', function(req, res, next) {
-   res.json({ msg: 'This is CORS-enabled for all origins!' });
-});
-app.head('/', function(req, res, next) {
-   res.json({ msg: 'This is CORS-enabled for all origins!' });
-});
-app.post('/', function(req, res, next) {
-   res.json({ msg: 'This is CORS-enabled for all origins!' });
-});
-app.put('/', function(req, res, next) {
-   res.json({ msg: 'This is CORS-enabled for all origins!' });
-});
+// app.head('/', function(req, res, next) {
+//    res.json({ msg: 'This is CORS-enabled for all origins!' });
+// });
+// app.post('/', function(req, res, next) {
+//    res.json({ msg: 'This is CORS-enabled for all origins!' });
+// });
+// app.put('/', function(req, res, next) {
+//    res.json({ msg: 'This is CORS-enabled for all origins!' });
+// });
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
